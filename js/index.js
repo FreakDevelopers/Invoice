f325dd19cd8c4ecc87d2b1b2f50cbdfcacfa276c
webdeveloper-amrut <webdeveloper.amrut@gmail.com>
2022-12-21 22:23:16 +0530

    feat: amount calculation

--- js/index.js
+++ js/index.js
@@ -2,25 +2,30 @@
 var count = 0;
 function addComponent() {
   var i = ++count;
+
   // Creation of Divs
   var div = document.createElement("div");
   div.className = "component";
   div.id = "component" + i;
+
   var divwc = document.createElement("div");
   divwc.className = "wrap-column";
+
   var divwr = document.createElement("div");
   divwr.className = "wrap-row";
 
   // Creation of input od wrap-column
   var itemdesc = document.createElement("input");
-  itemdesc.id = "itemdesc";
+  itemdesc.id = "itemdesc" + i;
   itemdesc.type = "text";
   itemdesc.name = "itemdesc";
   itemdesc.placeholder = "Item Description";
+
   var itemdetails = document.createElement("textarea");
-  itemdetails.id = "itemdetails";
+  itemdetails.id = "itemdetails" + i;
   itemdetails.rows = "3";
   itemdetails.cols = "25";
+  itemdetails.style.width = "100%";
   itemdetails.name = "itemdetails";
   itemdetails.placeholder = "Additional Details";
   itemdetails.style.padding = "0.7rem 1rem";
@@ -28,20 +33,31 @@ function addComponent() {
 
   // Creation of input od wrap-row
   var rate = document.createElement("input");
-  rate.id = "rate";
+  rate.id = "rate" + i;
+  rate.className = "position1";
   rate.type = "text";
   rate.name = "rate";
+  rate.setAttribute("onchange", "addFunction(this,"+ i +")");
   rate.placeholder = "₹0.00";
+
   var quantity = document.createElement("input");
-  quantity.id = "quantity";
-  quantity.type = "text";
+  quantity.id = "quantity" + i;
+  quantity.className = "position2";
+  quantity.type = "number";
+  quantity.min = "1";
   quantity.name = "quantity";
-  quantity.placeholder = "0";
+  quantity.value = 1;
+  quantity.setAttribute("onchange", "addFunction(this,"+ i +")");
+
+
   var amount = document.createElement("input");
-  amount.id = "amount";
+  amount.id = "amount" + i;
+  amount.className = "position3";
   amount.type = "text";
+  amount.readOnly = true;
   amount.name = "amount";
   amount.placeholder = "₹0.00";
+
   var deletebtn = document.createElement("input");
   deletebtn.id = "deletebtn" + i;
   deletebtn.className = "deletebtn";
@@ -61,6 +77,20 @@ function addComponent() {
   divwr.appendChild(deletebtn);
 }
 
+// Amount calculation
+var addRate = 0;
+var addQuantity = 1;
+function addFunction(input,i){
+  var sum = document.getElementById('amount' + i);
+  var rt = document.getElementById('rate' + i).value;
+  var qty = document.getElementById('quantity' + i).value;
+  if(input.type=="text" || input.type=="number"){
+    addRate = parseInt(rt);
+      addQuantity=parseInt(qty);
+  }
+  sum.value=(addRate*addQuantity);
+}
+
 // Delete Component
 function deleteComponent(ev) {
   var deletebtnid = ev.target.id;
@@ -71,18 +101,29 @@ function deleteComponent(ev) {
 }
 
 // Pop-Up window for invoice print
-function newWindow(){
+function newWindow() {
   window.open("invoice.html", "invoice", "popup");
 }
 
 // Reset all fields
-function resetFields(){
-  let x=window.confirm('Are you sure?');
-  console.log(x);
-  if(x==true){
-    for(i=0; i<=10; i++){
-      var abc = document.getElementsByTagName('input')[i];
-      console.log(abc);
+function resetFields() {
+  let x = window.confirm("Are you sure?");
+  if (x == true) {
+    document.getElementById("cname").value = "";
+    document.getElementById("cemail").value = "";
+    document.getElementById("ccontact").value = "";
+    document.getElementById("caddress").value = "";
+    document.getElementById("city-state").value = "";
+    document.getElementById("zipcode").value = "";
+    document.getElementById("invoiceno").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("note").value = "";
+    for (i = 1; i <= count; i++) {
+      document.getElementById("itemdesc" + i).value = "";
+      document.getElementById("itemdetails" + i).value = "";
+      document.getElementById("rate" + i).value = "";
+      document.getElementById("quantity" + i).value = "";
+      document.getElementById("amount" + i).value = "";
     }
   }
 }
